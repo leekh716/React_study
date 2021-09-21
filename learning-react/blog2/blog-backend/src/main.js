d9f752f97694827bbce82c13fb796c9266c6aaca
kunlee <leekh716@naver.com>
2021-09-21 16:54:52 +0900

    react blog backend

--- learning-react/blog2/blog-backend/src/main.js
+++ learning-react/blog2/blog-backend/src/main.js
@@ -4,6 +4,7 @@ import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 import api from './api';
+import jwtMiddleware from './lib/jwtMiddleware';
 
 const { PORT, MONGO_URI } = process.env;
 
@@ -22,6 +23,8 @@ const router = new Router();
 
 router.use('/api', api.routes());
 app.use(bodyParser());
+app.use(jwtMiddleware);
+
 app.use(router.routes()).use(router.allowedMethods());
 
 const port = PORT || 4000;
