b726e8b7b14e61cbb072a5244f3b88006cac07a0
kunlee <leekh716@naver.com>
2021-09-24 22:27:39 +0900

    react blog login register api

--- learning-react/blog2/blog-frontend/src/components/auth/AuthForm.js
+++ learning-react/blog2/blog-frontend/src/components/auth/AuthForm.js
@@ -48,18 +48,33 @@ const textMap = {
 	register: '회원가입',
 };
 
-const AuthForm = ({ type }) => {
+const ErrorMessage = styled.div`
+	color: red;
+	text-align: center;
+	font-size: 0.875rem;
+	margin-top: 1rem;
+`;
+
+const AuthForm = ({ type, form, onChange, onSubmit, error }) => {
 	const text = textMap[type];
 	return (
 		<AuthFormBlock>
 			<h3>{text}</h3>
-			<form>
-				<StyledInput autoComplete="username" name="username" placeholder="아이디" />
+			<form onSubmit={onSubmit}>
+				<StyledInput
+					autoComplete="username"
+					name="username"
+					placeholder="아이디"
+					onChange={onChange}
+					value={form.username}
+				/>
 				<StyledInput
 					autoComplete="new-password"
 					name="password"
 					placeholder="비밀번호"
 					type="password"
+					onChange={onChange}
+					value={form.password}
 				/>
 				{type === 'register' && (
 					<StyledInput
@@ -67,8 +82,11 @@ const AuthForm = ({ type }) => {
 						name="passwordConfirm"
 						placeholder="비밀번호 확인"
 						type="password"
+						onChange={onChange}
+						value={form.passwordConfirm}
 					/>
 				)}
+				{error && <ErrorMessage>{error}</ErrorMessage>}
 				<ButtonWithMarginTop cyan fullWidth>
 					{text}
 				</ButtonWithMarginTop>
--- learning-react/blog2/blog-frontend/src/index.js
+++ learning-react/blog2/blog-frontend/src/index.js
@@ -4,11 +4,18 @@ import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from './modules/index';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer, { rootSaga } from './modules/index';
+import createSagaMiddleware from 'redux-saga';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer, composeWithDevTools());
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
